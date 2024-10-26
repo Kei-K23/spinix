45383220767adbcc676edc702e7674d8458c1b75
Kei-K23 <keiksl2301@gmail.com>
2024-10-26 20:13:14 +0630

    Implement main struct for progress bar element

--- progress-bar.go
+++ progress-bar.go
@@ -1,6 +1,25 @@
 package termloader
 
-type ProgressBar struct{}
+import (
+	"sync"
+	"time"
+)
+
+type ProgressBar struct {
+	width          int              // Width of the progress bar
+	progress       int              // Current progress percentage
+	barChar        string           // Character to display a filled part
+	emptyChar      string           // Character to display a empty part
+	leftBorder     string           // Character to use as progress left border
+	rightBorder    string           // Character to use as progress right border
+	color          string           // Color for progress bar
+	label          string           // Optional label text to show
+	showLabel      bool             // Flag to show or hide label
+	showPercentage bool             // Flag to show or hide progress percentage
+	speed          time.Duration    // Progress bar speed
+	mutex          sync.Mutex       // For thread safe
+	stopCh         chan interface{} // Signal to send to stop the go routine and stop the progress bar
+}
 
 func NewProgressBar() *ProgressBar {
 	return &ProgressBar{}
